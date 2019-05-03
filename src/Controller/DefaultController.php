ca122ded3f701a66d5e91776b566a40de7c0296e
Crinos33 <fernandezjeanphilippe@gmail.com>
2019-05-03 15:36:29 +0200

    code sain

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,35 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-/**
- * @Route("/")
- */
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig',[
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR
+
+        ]);
     }
+
+    /**
+     * @Route("/admin", name="homeAdmin")
+     */
+    //@IsGranted("ROLE_ADMIN")                                                      Autre facon de proteger.
+    public function indexAdmin()
+    {
+        //$this->denyAccessUnlessGranted("ROLE_ADMIN");                             Autre facon de proteger trop noob
+        return $this->render('default/index.html.twig',[
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR
+
+    ]);
+    }
+
+
 }
+
---
+++ src/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @ORM\Entity
+ * @ORM\Table(name="for_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
